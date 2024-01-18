47618ed470d5b28eedbfe83e8bcfeba651650f24
cameron turner <17177161+cturner8@users.noreply.github.com>
2024-01-18 08:22:20 +0000

    setup initial mux api

--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,5 @@
 module cturner8/local-kv
 
 go 1.21.6
+
+require github.com/gorilla/mux v1.8.1
--- api/main.go
+++ api/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	log.Println("Running")
+	log.Println("Initializing API")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("API is running..."))
+	})
+
+	log.Println("API is running...")
+
+	// Bind to a port and pass our router in
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
