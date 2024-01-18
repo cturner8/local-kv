4806b35faf199c3dad6e964b2d410839dce82616
cameron turner <17177161+cturner8@users.noreply.github.com>
2024-01-18 08:38:24 +0000

    initial KMS api operation handler

--- api/main.go
+++ api/main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+func ListKeysHandler(w http.ResponseWriter, r *http.Request) {
+	jsonData, err := json.Marshal([]string{})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(jsonData)
+}
+
+// Middleware function, which will be called for each request
+func HeadersMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	log.Println("Initializing API")
 
 	router := mux.NewRouter()
+	router.Use(HeadersMiddleware)
+
+	// API operations
+	router.HandleFunc("/", ListKeysHandler).Methods("POST").Headers("X-Amz-Target", "TrentService.ListKeys")
+
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("API is running..."))
 	})
